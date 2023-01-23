2b6c1e55336779c250cb610faf225e551a8d8f3e
CodyBones101 <iamaplsoz@gmail.com>
2023-01-22 19:14:26 -0500

    4th commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+ /*----- constants -----*//
+let letters;
+
+let lives;
+
+
+
+
+init ()
+// Obtain randomly generated word from word list function
+
+
+// random word will be selected upon every reset and init
+let select_word;
+
+ /*----- constants -----*/
+
+
+  /*----- state variables -----*/
+// random word will be selected upon every reset and init
+let select_word;
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
+  // Obtain randomly generated word from word list function
+  const getRandomWord = function (list) {
+    return list[Math.floor(Math.random() * word_list.length)];
+};
