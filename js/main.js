b5f202460aeffaf2bb6a1a7eb03bca2366d1b45c
CodyBones101 <iamaplsoz@gmail.com>
2023-01-25 19:23:06 -0500

    added hint button functionality

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
  /*----- constants -----*/
 
 
-const WORD_BANK = ['ATMOSPHERE','ELECTROMAGNETIC SPECTRUM', 'GALAXY', 'UNIVERSE', 'GRAVITY', 'NEBULA', 'SPACECRAFT', 'SUPERNOVA', 'EXOPLANET', 'GAMMARAY'];
-
+const WORD_BANK = ['ATMOSPHERE','ELECTROMAGNETIC', 'GALAXY', 'UNIVERSE', 'GRAVITY', 'NEBULA', 'SPACECRAFT', 'SUPERNOVA', 'EXOPLANET', 'GAMMARAY'];
+const HINTS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
  /*----- constants -----*/
 
   /*----- state variables -----*/
@@ -11,31 +11,36 @@ let numberLives;
 let wrongGuesses;
 let secretWord;
 let gameStatus;
+let gameHint;
   
   /*----- cached elements  -----*/
 const letterEls = document.getElementById('letters');
 const answerBoxEls = document.getElementById('answer-spaces');
-const hintClick = document.getElementById('hint-btn');
+const hintButton = document.getElementById('hint-btn');
 const spaceMan = document.querySelector('img');
 const numLiveEl = document.getElementById("numberLives");
 const message = document.querySelector('.msg');
 const playButton = document.querySelector('.notify-btn');
+// const letterStyle = document.querySelectorAll('#letters' > 'button');
 
 
   /*----- event listeners -----*/
 letterEls.addEventListener('click', handleClick) 
 answerBoxEls.addEventListener('click', handleClick)
-hintClick.addEventListener('click', handleClick)
+hintButton.addEventListener('click', handleHint)
 document.getElementById('letters').addEventListener('click', handleClick)
 playButton.addEventListener('click', init)
   /*----- functions -----*/
   
 function init() {
-    secretWord = WORD_BANK[Math.floor(Math.random() * WORD_BANK.length)].split('');
+    let idx = Math.floor(Math.random() * WORD_BANK.length);
+    secretWord = WORD_BANK[idx].split('');
+    gameHint = HINTS[idx];
     wrongGuesses = [];
     wordStatus = secretWord.map(ltr => ' _ ');
     gameStatus = null;
     numberLives = 6;
+    document.getElementById('hint').textContent = "";
     render();
 }
 
@@ -60,7 +65,7 @@ function handleClick (evt) {
         } 
         if (numberLives === 0) {
             gameStatus = "L"
-        } 
+        }
     }
     render();
 }
@@ -84,13 +89,22 @@ function render() {
 
 function renderMessage() {
     if (gameStatus === "L") {
-        message.textContent = "Unfortunately, this isn't your day, we're so sorry but prepare to board spacecraft for return to earth."
+        message.textContent = "Unfortunately, this isn't your day, we're so sorry, but prepare to board spacecraft for return to earth."
     } else if(gameStatus === "W") {
-        message.textContent = "STELLAR, you've conquered the galaxy and Elon Musk himself has crowned you Supreme leader of Mars!"
+        message.textContent = "STELLAR, you've conquered the galaxy and have been crowned by Elon Musk as Supreme leader of Mars!"
     } else {
         message.textContent = ""
     }
 }
+
+function handleHint() {
+    document.getElementById('hint').textContent = gameHint;
+
+}
+
+// function renderButtonStyle() {
+//     letterStyle.forEach()
+// }
 
 
 
